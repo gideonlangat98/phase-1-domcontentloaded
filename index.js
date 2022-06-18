7b70a13c9777b20503780bd0e32b02f1c1f2b47c
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-18 12:35:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
+const text = document.getElementById("text");
 document.addEventListener('DOMContentLoaded', function(){
-   const text = document.getElementById("text");
    text.innerHTML = 'This is really cool!'
 })
